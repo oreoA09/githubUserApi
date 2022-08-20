fcf04638db0c39c789fda127f671e817cfe29e5c
Oreo <45500876+oreoA09@users.noreply.github.com>
2022-08-20 11:42:41 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+function getRepos(username) {
+	fetch(`https://api.github.com/users/${username}/repos`)//${}is a template string
+		.then(response => response.json())
+		.then(responseJson => displayRepos(responseJson));
+}
+
+function displayRepos(responseJson) {
+	console.log(responseJson)
+	//reads the data from the fetch and creates an html
+	$("#results").html("")//clears the results div
+	responseJson.forEach(item => {//loops thru the arr
+		$("#results").append(`
+    <div class="resultsrow"><h2><a href="${item.html_url}">${item.full_name}</a></h2> <p>Forks: ${item.forks}</p></div>
+    `)//appends image to results div
+	})
+}
+
+function watchForm() {
+	$('#form').submit(event => {
+		event.preventDefault();
+		const username = event.target.username.value
+		// console.log(username);
+		getRepos(username);
+	});
+}
+
+$(function() {
+	console.log('App loaded! Waiting for submit!');
+	watchForm();
+});
